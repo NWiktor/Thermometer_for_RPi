6f141b6cab73d7e6bfb2f41da96b3bbf16a100ee
NWiktor <31969508+NWiktor@users.noreply.github.com>
2023-04-05 18:58:01 +0200

    replace indentation with spaces, add prints, and fix issue with copy src path

--- file_collector.py
+++ file_collector.py
@@ -16,37 +16,46 @@ Wetzl Viktor - 2023.03.25 - All rights reserved
 
 import time
 import os
-import sys
 import shutil
 
 
 def main():
-	files = []
+    """ Copy and rename all '.png' files in the starting directory
+    to a subfolder named: 'database'.
+    """
+    files = []
 
-	# List files
-	for file in os.listdir():
-		if file.endswith(".png"):
-			files.append(file)
+    # List files
+    for file in os.listdir(INITDIR):
+        if file.endswith(".png"):
+            files.append(file)
 
-	if files is None:
-		return
+    if files is None:
+        print("No '.png' found!")
+        return
 
-	# Create folder
-	try:
-		os.mkdir(DATABASE_PATH)
+    # Create folder
+    try:
+        os.mkdir(DATABASE_PATH)
+        print("Folder created")
 
-	except FileExistsError:
-		print("Folder already exists!")
+    except FileExistsError:
+        print("Folder already exists!")
 
-	# Copy and rename files
-	for file in files:
-		# filename = f"{DATE}_{file}"
-		filename = "{0}_{1}".format(DATE, file)
-		shutil.copy2(file, os.path.join(DATABASE_PATH, filename))
+    # Copy and rename files
+    try:
+        for file in files:
+            filename = f"{DATE}_{file}"
+            src = os.path.join(INITDIR, file)
+            dst = os.path.join(DATABASE_PATH, filename)
+            shutil.copy2(src, dst)
+
+    except Exception as exc:
+        print(exc)
 
 
 if __name__ == '__main__':
-	DATE = time.strftime("%Y-%m-%d", time.localtime())
-	INITDIR = os.path.dirname(__file__)
-	DATABASE_PATH = os.path.join(INITDIR, "database")
-	main()
+    DATE = time.strftime("%Y-%m-%d", time.localtime())
+    INITDIR = os.path.dirname(__file__)
+    DATABASE_PATH = os.path.join(INITDIR, "database")
+    main()
